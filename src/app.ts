38011d61ba27c34f22777c9eff839d8c2014879c
o4codes <oforkansi.shadrach@gmail.com>
2024-01-22 05:36:55 +0100

    added logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { RegisterRoutes } from "../build/routes";
 import express, { json, urlencoded, Response as ExResponse, Request as ExRequest } from "express";
 import swaggerUi from "swagger-ui-express";
+import { logger } from "./configs/logger";
+import { httpLogger } from "./configs/httpLogger";
 
 export const app = express();
+app.use(httpLogger);
 
 // Use body parser to read sent json payloads
 app.use(
@@ -13,6 +16,7 @@ app.use(
 app.use(json());
 
 app.use("/api/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    logger.info("Serving Swagger UI");
     return res.send(
         swaggerUi.generateHTML(await import("../build/swagger.json"))
     );
