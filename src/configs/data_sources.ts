37bef0d0161b2713bdd8dfbd235b0dca75be3f44
o4codes <oforkansi.shadrach@gmail.com>
2024-01-22 22:58:01 +0100

    connection to db established

--- src/configs/data_sources.ts
+++ src/configs/data_sources.ts
@@ -1,5 +1,7 @@
 import { DataSource } from 'typeorm';
 import Settings from './settings';
+import { logger } from './logger';
+import { UserEntity, UserSecurityKeysEntity } from '../models';
 
 export const MainDataSource = new DataSource({
     type: 'postgres',
@@ -9,4 +11,16 @@ export const MainDataSource = new DataSource({
     password: Settings.dbPassword,
     database: Settings.dbName,
     synchronize: Settings.isDebug,
+    entities: [
+        UserEntity, UserSecurityKeysEntity
+    ],
 });
+
+
+MainDataSource.initialize()
+    .then(() => {
+        logger.info('Data Source has been initialized!');
+    })
+    .catch((err) => {
+        logger.error('Error during Data Source initialization:', err);
+    });
